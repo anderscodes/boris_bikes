81770287bb5e042f1e3a421a6e62c4f9d568f709
anderscodes <anders.j.persson@hotmail.com>
2018-04-18 13:22:27 +0100

    created unite test to gets a bike and expect the bike to be working and made it pass

--- spec/borris_bike_spec.rb
+++ spec/borris_bike_spec.rb
@@ -2,4 +2,10 @@ require 'borris_bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike}
+  it "Expects new instance of bike class" do
+  expect(Bike.new.working?).to eq true
+ #   # bike = subject.release_bike
+ #   # expect(bike).to be_working?
+  end
+
 end
