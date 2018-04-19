26727cd540f76d26ed6780b47b4bbfc37c64a903
anderscodes <anders.j.persson@hotmail.com>
2018-04-19 16:45:53 +0100

    set capacity to a default capacity

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,6 +6,15 @@ describe DockingStation do
   it "check bike working?" do
     expect(Bike.new.working?).to eq true
   end
+
+describe 'initialization' do
+  it "has a variable capacity" do
+    docking_station = DockingStation.new(5)
+    expect{ 5.times { subject.dock(bike) } }.to raise_error
+end
+
+
+
   it "bike docked" do
     bike = Bike.new
     expect(subject.dock(bike)).to eq [bike]
@@ -23,6 +32,7 @@ describe "#release_bike" do
     subject.dock(bike)
     expect(subject.release_bike).to eq bike
   end
+end
 
   it "raise error if no bikes are docked" do
     # bike = nil
